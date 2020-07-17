06e979495d4eb569042663541f5787eb11af8fdb
ashokranera <ashokranera@gmail.com>
2020-07-17 17:05:45 +0530

    Design activity_main.xml , Add CardView And Other Component

--- app/src/main/java/com/friends/covid19/activity/MainActivity.java
+++ app/src/main/java/com/friends/covid19/activity/MainActivity.java
@@ -1,8 +1,11 @@
-package com.friends.covid19;
+package com.friends.covid19.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.friends.covid19.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void goTrackCountries(View view) {
     }
 }
